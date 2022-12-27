de13a0012829a66ac0f223af46a413dc5492011d
anapaulalunkes <anapaulalunkes22@gmail.com>
2022-12-27 15:17:02 -0300

    first commit

---
+++ lampada.js
@@ -0,0 +1,28 @@
+const turnOn = document.getElementById("turnOn");
+const turnOf = document.getElementById("turnOf");
+const lamp = document.getElementById("lamp");
+
+function isLampBroken() {
+  return lamp.src.indexOf ("quebrada") > -1; // indexOf faz uma varedura numa string procurando outro trecho de string
+}
+
+function lampOn() {
+  if ( !isLampBroken() ) {
+    lamp.src = "./img/ligada.jpg";
+  }
+}
+function lampOf() {
+  if ( !isLampBroken() ) {
+    lamp.src = "./img/desligada.jpg";
+  }
+}
+
+function lampBroken() {
+  lamp.src = "./img/quebrada.jpg";
+}
+
+turnOn.addEventListener("click", lampOn);
+turnOf.addEventListener("click", lampOf);
+lamp.addEventListener("mouseover", lampOn);
+lamp.addEventListener("mouseleave", lampOf);
+lamp.addEventListener("dblclick", lampBroken);
